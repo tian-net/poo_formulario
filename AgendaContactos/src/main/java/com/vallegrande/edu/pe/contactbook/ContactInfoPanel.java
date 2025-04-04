acc1215841cda039806ef0f27607da95d3f75a27
ValeryGiselleChumpitazCaycho <111779657+ValeryChumpitaz@users.noreply.github.com>
2025-04-04 15:33:14 -0500

    Update ContactInfoPanel.java

--- AgendaContactos/src/main/java/com/vallegrande/edu/pe/contactbook/ContactInfoPanel.java
+++ AgendaContactos/src/main/java/com/vallegrande/edu/pe/contactbook/ContactInfoPanel.java
@@ -6,4 +6,17 @@ import java.awt.*;
 
 public class ContactInfoPanel extends JPanel {
 
+    private static JLabel lblInfo;
+public ContactInfoPanel() {
+    setLayout(new BorderLayout());
+    lblInfo = new JLabel("Gestiona tus contactos facilmente");
+    lblInfo.setBorder(new EmptyBorder(15, 0, 15, 0));
+    lblInfo.setFont(new Font("Tahoma", Font.BOLD, 16));
+    lblInfo.setHorizontalAlignment(SwingConstants.CENTER);
+    add(lblInfo,BorderLayout.CENTER);
+ }
+public static void actualizarInfo(String info) {
+int totalContacts = ContactListPanel.getContactCount();
+lblInfo.setText("Total de Contactos" + totalContacts);
+}
 }
