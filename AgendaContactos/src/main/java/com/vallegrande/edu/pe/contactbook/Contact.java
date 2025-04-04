7ea9872e95b5cd42f39ba22cf6b1058da3a0be82
ValeryGiselleChumpitazCaycho <111779657+ValeryChumpitaz@users.noreply.github.com>
2025-04-04 15:23:43 -0500

    Update Contact.java

--- AgendaContactos/src/main/java/com/vallegrande/edu/pe/contactbook/Contact.java
+++ AgendaContactos/src/main/java/com/vallegrande/edu/pe/contactbook/Contact.java
@@ -2,4 +2,36 @@ package com.vallegrande.edu.pe.contactbook;
 
 public class Contact {
 
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+ public String getName() {
+        return name;
+ }
+public void setName(String name) {
+        this.name = name;
+}
+public String getPhoneNumber() {
+        return phoneNumber;
+}
+public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+}
+public String getEmail() {
+        return email;
+}
+public void setEmail(String email) {
+        this.email = email;
+}
+@Override
+public String toString() {
+        return name + "|" + phoneNumber + "|" + email;
+}
 }
